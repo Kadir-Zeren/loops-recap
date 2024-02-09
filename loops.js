c52705a14c51065b1bc71f78b94e09957485feda
kadir-zeren <k4dzeren@gmail.com>
2024-02-09 14:40:59 +0300

    random

--- loops.js
+++ loops.js
@@ -1,19 +1,19 @@
-for (let i = 0; i < 100; i++) {
-  console.log(`${i}-2024`);
-}
+// for (let i = 0; i < 100; i++) {
+//   console.log(`${i}-2024`);
+// }
 
-const n = prompt(`enter your number`);
+// const n = prompt(`enter your number`);
 
-let sum = 0;
-for (let i = 1; i <= n; i++) {
-  sum = sum + i;
-  console.log(sum);
-}
-console.log(`sum: ${sum}`);
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
+// console.log(`sum: ${sum}`);
 
 const number = prompt(`how many numbers:?`);
 
 for (let sayac = 1; sayac <= number; sayac++) {
-  const rasgeleSayi = Math.random();
-  console.log(rasgeleSayi);
+  const rasgeleSayi = Math.round(Math.random() * 100);
+  console.log(`${sayac}. sayiniz : ${rasgeleSayi} `);
 }
